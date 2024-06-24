c9b9398cd6eb1a5ed038710920defe2f273cb110
Yunatico <fr@fr.fr>
2024-06-24 17:13:13 +0200

    Ajout image background

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet, PDFViewer, Font } from '@react-pdf/renderer';
+import myImage from '../assets/PN-FOND-A3-240624-BD.jpg';
+import { Page, Text, View, Document, StyleSheet, PDFViewer, Font, Image } from '@react-pdf/renderer';
 
 // Register font
 Font.register({
@@ -11,7 +12,15 @@ const styles = StyleSheet.create({
 	page: {
 		flexDirection: 'column',
 		backgroundColor: '#E4E4E4',
-		width: '100%'
+		width: '100%',
+		backgroundImage: `url(${myImage})`,
+		margin: 0
+	},
+	backgroundImage: {
+		position: 'absolute',
+		width: '100%',
+		height: '100%',
+		zIndex: 'auto'
 	},
 	section: {
 		margin: 10,
@@ -21,12 +30,31 @@ const styles = StyleSheet.create({
 		fontFamily: 'Montserrat',
 		fontWeight: 600,
 		fontSize: '60pt'
+	},
+	text: {
+		position: 'absolute',
+		left: '0px',
+		right: '0px',
+		marginHorizontal: 'auto',
+		textAlign: 'center',
+		justifyContent: 'center'
 	}
 });
 
 const MyDocument = ({ data }) => (
 	<Document>
 		<Page size='A3' style={styles.page}>
+			<View>
+				<Image
+					style={{
+						height: '99%',
+						width: '99%',
+						marginHorizontal: 'auto'
+					}}
+					src={myImage}
+					alt=''
+				></Image>
+			</View>
 			<View style={styles.section}>
 				<Text>Titre {data.titre}</Text>
 				<Text>Texte {data.texte}</Text>
